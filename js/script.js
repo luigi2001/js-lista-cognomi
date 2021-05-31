4b6dacbf421fefeb2cd1a0524d431e0fd865605d
luigi2001 <luigitanga@virgilio.it>
2021-05-31 18:23:41 +0200

    correzione primo esercizio

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 
+/* primo esercizio  */
+
 var mail = prompt('Inserisci la tua mail:');
 console.log (mail);
 
 var iscritti = ['mariorossi12@gmail.com', 'luca.verdi@virgilio.it', 'giovannicairoli@libero.it', 'marcoambrosio@yahoo.com', 'guido.bianchi@gmail.com'];
 
+var risposta = 'la mail inserita non risulta nei nostri database';
+
 var i = 0;
 
 while (i < iscritti.length){
     if (iscritti[i] == mail){
-        console.log ('presente');
+        console.log (risposta = 'mail trovata');
     } else {
-        console.log ('non presente');
+        console.log (risposta);
     }
-    break;
     i++;
 }
+
+document.getElementById('risposta').innerHTML = risposta;
