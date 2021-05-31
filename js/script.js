600dbcd2da65f1703f25820353862ec7269d48e5
luigi2001 <luigitanga@virgilio.it>
2021-05-31 17:55:45 +0200

    add primo esercizio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log ('ciao');
+
+var mail = prompt('Inserisci la tua mail:');
+console.log (mail);
+
+var iscritti = ['mariorossi12@gmail.com', 'luca.verdi@virgilio.it', 'giovannicairoli@libero.it', 'marcoambrosio@yahoo.com', 'guido.bianchi@gmail.com'];
+
+var i = 0;
+
+while (i < iscritti.length){
+    if (iscritti[i] == mail){
+        console.log ('presente');
+    } else {
+        console.log ('non presente');
+    }
+    break;
+    i++;
+}
